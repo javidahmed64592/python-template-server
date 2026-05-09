80e8ad1d03d64983a745111fc4ba0c7b4d3036cc
Javid Ahmed <javidahmed@icloud.com>
2026-05-09 02:43:22 +0100

    Refactor BaseDatabaseManager initialization and update tests for configuration

--- python_template_server/db/base_database_manager.py
+++ python_template_server/db/base_database_manager.py
@@ -14,10 +14,15 @@ class BaseDatabaseManager(ABC):
     """Manager class for database operations.
 
     Subclasses must implement the `db_url` property to provide the correct database URL.
+    The database manager must be configured using `configure()` after the server has loaded the configuration.
     """
 
-    def __init__(self, db_config: DatabaseConfig) -> None:
+    def __init__(self) -> None:
         """Initialize the database manager."""
+        logger.info("Ready to be configured...")
+
+    def configure(self, db_config: DatabaseConfig) -> None:
+        """Configure the database manager with the given configuration."""
         self.db_config = db_config
         self.db_config.db_directory.mkdir(parents=True, exist_ok=True)
 
