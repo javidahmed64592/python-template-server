4c7a038ceccc4126a9de3fd88493d376867261e8
Javid Ahmed <57677851+javidahmed64592@users.noreply.github.com>
2026-05-15 02:00:32 +0100

    Add logging for configured routes in TemplateServer (#100)
    * Add logging for configured routes in TemplateServer
    
    * Update dependencies
    
    * Add type ignore comment for router routes in logging

--- python_template_server/template_server.py
+++ python_template_server/template_server.py
@@ -284,6 +284,10 @@ class TemplateServer(ABC):
             self.app.mount("/", StaticFiles(directory=self.static_dir, html=True), name="static")
             self.app.add_exception_handler(StarletteHTTPException, self._custom_404_handler)  # type: ignore[arg-type]
 
+        for router in routers:
+            routes = [route.path for route in router.router.routes]  # type: ignore[attr-defined]
+            logger.info("Configured routes for %s: %s", router.__class__.__name__, routes)
+
     def run(self) -> None:
         """Run the server using uvicorn."""
         try:
